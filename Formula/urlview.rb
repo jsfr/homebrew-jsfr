2b02b02d46f91980ca02905da6a1011ae03003ee
Jens Fredskov <jensfredskov@protonmail.ch>
2018-01-03 11:34:52 +0100

    Add head

--- Formula/urlview.rb
+++ Formula/urlview.rb
@@ -2,8 +2,9 @@ class Urlview < Formula
   desc "URL extractor/launcher"
   homepage "https://github.com/sigpipe/urlview"
   url "https://github.com/sigpipe/urlview/archive/08767aa863cd27d1755ba0aff65b8cc1a0c1446a.tar.gz"
-  version "0.9-21"
   sha256 "23d32e7f610601f012e06d79b206d4952259bdabe9aa64940c7e1bb8c0cb6519"
+  version "0.9-21"
+  head "https://github.com/sigpipe/urlview.git"
 
   def install
     inreplace "urlview.man", "/etc/urlview/url_handler.sh", "open"
